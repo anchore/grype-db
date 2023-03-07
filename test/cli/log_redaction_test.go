0878d21c23c208c499bd969cfb699b412a062841
Alex Goodman <alex.goodman@anchore.com>
2023-03-07 14:27:20 -0500

    remove extra CLI test fields
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- test/cli/log_redaction_test.go
+++ test/cli/log_redaction_test.go
@@ -8,10 +8,8 @@ import (
 func TestLogRedaction_CommandConfigs(t *testing.T) {
 	secretValue := "topsy-kretts"
 	tests := []struct {
-		name       string
-		args       []string
-		env        map[string]string
-		assertions []traitAssertion
+		name string
+		args []string
 	}{
 		{name: "pull"},
 		{name: "build"},
@@ -22,12 +20,11 @@ func TestLogRedaction_CommandConfigs(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			args := append(strings.Split(test.name, " "), "--dry-run", "-vv", "-c", "test-fixtures/grype-db-config-with-secrets.yaml")
 			args = append(args, test.args...)
-			cmd, stdout, stderr := runGrypeDB(t, test.env, args...)
+			cmd, stdout, stderr := runGrypeDB(t, nil, args...)
 			assertions := []traitAssertion{
 				assertLoggingLevel("debug"),
 				assertNotInOutput(secretValue),
 			}
-			assertions = append(assertions, test.assertions...)
 			for _, traitFn := range assertions {
 				traitFn(t, stdout, stderr, cmd.ProcessState.ExitCode())
 			}
