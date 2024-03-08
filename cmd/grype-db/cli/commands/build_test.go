6bcd6a95933ee2f8058fbf7a335856789e7ddf35
Alex Goodman <wagoodman@users.noreply.github.com>
2024-03-08 09:48:39 -0500

    revert db age (#256)
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- cmd/grype-db/cli/commands/build_test.go
+++ cmd/grype-db/cli/commands/build_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/anchore/grype-db/pkg/provider"
 )
 
-func Test_latestTimestamp(t *testing.T) {
+func Test_earliestTimestamp(t *testing.T) {
 	tests := []struct {
 		name   string
 		states []provider.State
@@ -18,22 +18,22 @@ func Test_latestTimestamp(t *testing.T) {
 			name: "happy path",
 			states: []provider.State{
 				{
+					Timestamp: time.Date(2021, 1, 2, 0, 0, 0, 0, time.UTC),
+				},
+				{
 					Timestamp: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
 				},
 				{
 					Timestamp: time.Date(2021, 1, 3, 0, 0, 0, 0, time.UTC),
 				},
-				{
-					Timestamp: time.Date(2021, 1, 2, 0, 0, 0, 0, time.UTC),
-				},
 			},
-			want: time.Date(2021, 1, 3, 0, 0, 0, 0, time.UTC),
+			want: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := latestTimestamp(tt.states); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("latestTimestamp() = %v, want %v", got, tt.want)
+			if got := earliestTimestamp(tt.states); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("earliestTimestamp() = %v, want %v", got, tt.want)
 			}
 		})
 	}
