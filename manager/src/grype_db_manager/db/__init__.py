22df94d8ff7afa8ff2ce15d4aff33d6239ed13dc
Will Murphy <willmurphyscode@users.noreply.github.com>
2024-09-23 16:01:56 -0400

    chore: remove unused validation code
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>

--- manager/src/grype_db_manager/db/__init__.py
+++ manager/src/grype_db_manager/db/__init__.py
@@ -1,14 +1,13 @@
 from . import listing, metadata, schema
 from .listing import Listing
 from .metadata import Metadata
-from .validation import capture_results, validate
+from .validation import capture_results
 
 __all__ = [
     "Listing",
     "Metadata",
     "listing",
     "metadata",
-    "validate",
     "schema",
     "capture_results",
 ]
