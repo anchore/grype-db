79a2a74e9234397276d6526a24a157de19076f18
Ori Zerah <ori.zerah@echohq.com>
2025-05-07 16:46:35 +0300

    add echo
    Signed-off-by: Ori Zerah <ori.zerah@echohq.com>

--- manager/src/grype_db_manager/grypedb.py
+++ manager/src/grype_db_manager/grypedb.py
@@ -35,6 +35,7 @@ DB_DIR = "dbs"
 # For the meantime this is a snapshot of the expected namespaces for vunnel 0.17.2 in Oct 2023 (boo! 👻).
 v5_additional_namespaces = [
     "mariner:distro:azurelinux:3.0",
+    "echo:distro:echo:1",
 ]
 
 v4_expected_namespaces = [
