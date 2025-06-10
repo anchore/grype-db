63023343cfe9a3b35f3d2b84995c1264e6152ce6
Will Murphy <willmurphyscode@users.noreply.github.com>
2025-06-10 11:09:49 -0400

    test: echo namespace not expect in v5 yet
    Signed-off-by: Will Murphy <willmurphyscode@users.noreply.github.com>

--- manager/src/grype_db_manager/grypedb.py
+++ manager/src/grype_db_manager/grypedb.py
@@ -35,7 +35,6 @@ DB_DIR = "dbs"
 # For the meantime this is a snapshot of the expected namespaces for vunnel 0.17.2 in Oct 2023 (boo! 👻).
 v5_additional_namespaces = [
     "mariner:distro:azurelinux:3.0",
-    "echo:distro:echo:1",
 ]
 
 v4_expected_namespaces = [
